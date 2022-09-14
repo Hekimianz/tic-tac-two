28c192956a22870e99efe620dd20e756da269c1d
Aram Hekimian <aramhek121@gmail.com>
2022-09-13 19:43:05 -0500

    Implemented some game logic

--- app.js
+++ app.js
@@ -7,6 +7,7 @@
             '', '', ''];
 
 
+
         return gameBoardArray;
     })();
 
@@ -28,6 +29,91 @@
                     }
                     gameTile[i].innerHTML = gameBoard[i];
                 }
+                // Horizontal win conditions
+                if (gameBoard[0] == 'X' && gameBoard[1] == 'X' && gameBoard[2] == 'X') {
+
+                    gameBoard.forEach((element, index) => {
+                        gameBoard[index] = '';
+                    })
+                    console.log(gameBoard);
+                }
+                else if (gameBoard[3] == 'X' && gameBoard[4] == 'X' && gameBoard[5] == 'X') {
+                    gameBoard.forEach((element, index) => {
+                        gameBoard[index] = '';
+                    })
+                }
+                else if (gameBoard[6] == 'X' && gameBoard[7] == 'X' && gameBoard[8] == 'X') {
+                    gameBoard.forEach((element, index) => {
+                        gameBoard[index] = '';
+                    })
+                }
+                else if (gameBoard[0] == 'O' && gameBoard[1] == 'O' && gameBoard[2] == 'O') {
+                    gameBoard.forEach((element, index) => {
+                        gameBoard[index] = '';
+                    })
+                }
+                else if (gameBoard[3] == 'O' && gameBoard[4] == 'O' && gameBoard[5] == 'O') {
+                    gameBoard.forEach((element, index) => {
+                        gameBoard[index] = '';
+                    })
+                }
+                else if (gameBoard[6] == 'O' && gameBoard[7] == 'O' && gameBoard[8] == 'O') {
+                    gameBoard.forEach((element, index) => {
+                        gameBoard[index] = '';
+                    })
+                }
+                // Vertical win conditions
+                else if (gameBoard[0] == 'X' && gameBoard[3] == 'X' && gameBoard[6] == 'X') {
+                    gameBoard.forEach((element, index) => {
+                        gameBoard[index] = '';
+                    })
+                }
+                else if (gameBoard[1] == 'X' && gameBoard[4] == 'X' && gameBoard[7] == 'X') {
+                    gameBoard.forEach((element, index) => {
+                        gameBoard[index] = '';
+                    })
+                }
+                else if (gameBoard[2] == 'X' && gameBoard[5] == 'X' && gameBoard[8] == 'X') {
+                    gameBoard.forEach((element, index) => {
+                        gameBoard[index] = '';
+                    })
+                }
+                else if (gameBoard[0] == 'O' && gameBoard[3] == 'O' && gameBoard[6] == 'O') {
+                    gameBoard.forEach((element, index) => {
+                        gameBoard[index] = '';
+                    })
+                }
+                else if (gameBoard[1] == 'O' && gameBoard[4] == 'O' && gameBoard[7] == 'O') {
+                    gameBoard.forEach((element, index) => {
+                        gameBoard[index] = '';
+                    })
+                }
+                else if (gameBoard[2] == 'O' && gameBoard[5] == 'O' && gameBoard[8] == 'O') {
+                    gameBoard.forEach((element, index) => {
+                        gameBoard[index] = '';
+                    })
+                }
+                // Diagonal win conditions
+                else if (gameBoard[0] == 'X' && gameBoard[4] == 'X' && gameBoard[8] == 'X') {
+                    gameBoard.forEach((element, index) => {
+                        gameBoard[index] = '';
+                    })
+                }
+                else if (gameBoard[6] == 'X' && gameBoard[4] == 'X' && gameBoard[2] == 'X') {
+                    gameBoard.forEach((element, index) => {
+                        gameBoard[index] = '';
+                    })
+                }
+                else if (gameBoard[0] == 'O' && gameBoard[4] == 'O' && gameBoard[8] == 'O') {
+                    gameBoard.forEach((element, index) => {
+                        gameBoard[index] = '';
+                    })
+                }
+                else if (gameBoard[6] == 'O' && gameBoard[4] == 'O' && gameBoard[2] == 'O') {
+                    gameBoard.forEach((element, index) => {
+                        gameBoard[index] = '';
+                    })
+                }
 
             })
 
@@ -38,11 +124,9 @@
             })
             gameTile[i].addEventListener('mouseup', function () {
                 if (gameBoard[i] !== '') {
-                    gameTile[i].style.color = 'black';
+                    gameTile[i].style.color = 'transparent';
                 }
             })
-
-
         }
 
     })();
